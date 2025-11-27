2e3dfa62140f03df1737a42836e6af921388a480
Akul G <akulg005@gmail.com>
2025-11-26 23:31:46 -0500

    Code smell final postgres fix

--- listings-service/config.py
+++ listings-service/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    database_url: str = "postgresql://listings_user:listings_pass@localhost:5432/listings_db"
+    database_url: str
     auth_service_url: str = "http://localhost:8001"
     
     class Config:
